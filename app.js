0ec605cf2d973840a40ca04b2e7e92d0645a8856
Ninte <ninte.dev@gmail.com>
2021-07-29 11:19:13 +0100

    Added draft staff api routes.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const connectDB = require('./config/db');
 const router = require('./routes/api/staff');
 
@@ -9,9 +10,8 @@ const app = express();
 // connect database
 connectDB();
 
-app.get('/', (req, res) => res.send('Hello world'));
-
 const port = process.env.PORT || 8080;
 
 app.use(router);
+
 app.listen(port, () => console.log(`Server running on port ${port}`));
--- routes/api/staff.js
+++ routes/api/staff.js
@@ -5,4 +5,43 @@ const Staff = require('../../models/Staff');
 
 router.get('/test', (req, res) => res.send('testing Staff routes'));
 
+// get list of staff
+router.get('/', (req, res) => {
+  Staff.find()
+    .then((staff) => res.json(staff))
+    .catch((err) => res.status(404).json({ err: err + 'No staff found' }));
+});
+
+// add staff details
+router.post('/', (req, res) => {
+  console.log(req.body);
+  Staff.create()
+    .then((staff) => res.json({ message: 'Staff added successfully' }))
+    .catch((err) =>
+      res.status(400).json({ error: err + 'Unable to add staff' }),
+    );
+});
+
+router.get('/:id', (req, res) => {
+  Staff.findById(req.params.id)
+    .then((staff) => res.json(staff))
+    .catch((err) => {
+      res.status(404).json({ error: err + 'No staff found' });
+    });
+});
+
+router.put('/:id', (req, res) => {
+  Staff.findByIdAndUpdate(req.params.id)
+    .then((staff) => res.json({ message: 'Staff updated successfully' }))
+    .catch((err) =>
+      res.status(400).json({ error: err + 'Unable to update staff details' }),
+    );
+});
+
+router.delete('/:id', (req, res) => {
+  Staff.findByIdAndDelete(req.params.id)
+    .then((staff) => res.json({ message: 'Staff info deleted successfully' }))
+    .catch((err) => res.status(404).json({ error: err + 'Staff not found' }));
+});
+
 module.exports = router;
