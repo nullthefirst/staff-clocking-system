f67ab63135b9b8c672d3cf120fbb7a0833752528
Ninte <ninte.dev@gmail.com>
2021-07-29 20:41:01 +0100

    Added api for staff creation.

--- src/core/staffController.js
+++ src/core/staffController.js
@@ -2,6 +2,23 @@ const idCreator = require('../custom/idCreator');
 const Staff = require('./staffModel');
 
 // staff creation
+exports.create = async (req, res) => {
+  const _staff = new Staff();
+  _staff.name = req.body.name ? req.body.name : _staff.name;
+  _staff.staff_id = idCreator(req.body.name ? req.body.name : _staff.name);
+  _staff.clockIn = req.body.clockIn;
+  _staff.clockOut = req.body.clockOut;
+
+  try {
+    await _staff.save();
+    res.json({
+      message: 'New staff member added',
+      data: _staff,
+    });
+  } catch (err) {
+    res.json(err);
+  }
+};
 
 // display staff list
 exports.list = async (req, res) => {
