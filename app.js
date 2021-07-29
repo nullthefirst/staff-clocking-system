fbe55d47fe1bea7dae5658b484fd46945f033e31
Ninte <ninte.dev@gmail.com>
2021-07-29 09:48:06 +0100

    Completed database configuration and setup.

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
+require('dotenv').config();
+
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// connect database
+connectDB();
 
 app.get('/', (req, res) => res.send('Hello world'));
 
