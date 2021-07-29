4d34c739e0d7cc39829ec455af43a366b6cf2b48
Ninte <ninte.dev@gmail.com>
2021-07-29 15:55:55 +0100

    Restructured project.

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
 require('dotenv').config();
 
 const express = require('express');
-const bodyParser = require('body-parser');
-const connectDB = require('./config/db');
-const router = require('./routes/api/staff');
+// const staffRoutes = require('./src/apiRoutes');
+const mongoose = require('mongoose');
 
 const app = express();
 
 // connect database
-connectDB();
+const mongoURL = `mongodb+srv://nullthefirst:${process.env.MONGODB_PASSWORD}@releafdatabase.eai54.mongodb.net/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority`;
+mongoose.connect(mongoURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  dbName: process.env.MONGODB_DATABASE,
+});
+
+const db = mongoose.connection;
+
+if (!db) {
+  console.log('Error connecting to database');
+} else {
+  console.log('Successfully connected to database');
+}
+
+// setup middleware
+// app.use('/api', staffRoutes);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const port = process.env.PORT || 8080;
 
-app.use(router);
+app.get('/', (req, res) => res.send('Welcome to the Staff Clock-In System'));
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
---
+++ src/apiRoutes.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const router = express.Router();
+
+const Staff = require('./core/staffModel');
+const idCreator = require('./custom/idCreator');
