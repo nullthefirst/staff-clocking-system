973801b5dafc4c16735cd2335fff7262859ea9fd
Ninte <ninte.dev@gmail.com>
2021-07-30 11:24:43 +0100

    Setup react-router-dom. Moved homepage content to new component.

--- src/App.js
+++ src/App.js
@@ -1,47 +1,18 @@
 import React from 'react';
-import axios from 'axios';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Homepage from './components/Homepage';
+
 import './App.css';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      title: '',
-      hasLoaded: false,
-    };
-  }
-
-  componentDidMount() {
-    axios
-      .get('https://staff-clocking-system.herokuapp.com')
-      .then((res) => {
-        this.setState({
-          title: res.data,
-          hasLoaded: true,
-        });
-      })
-      .catch((err) => {
-        console.error(err);
-        console.log('Error fetching title');
-      });
-  }
-
   render() {
-    const titleText = this.state.hasLoaded
-      ? this.state.title
-      : 'Loading title ...';
-
     return (
-      <div className="container-fluid">
-        <div className="row" id="homepage">
-          <div className="col-md-2"></div>
-          <div className="col col-md-8">
-            <h1 className="text-color">{titleText}</h1>
-            <div className="my-5"></div>
-          </div>
-          <div className="col-md-2"></div>
+      <Router>
+        <div>
+          <Route exact path="/" component={Homepage} />
         </div>
-      </div>
+      </Router>
     );
   }
 }
