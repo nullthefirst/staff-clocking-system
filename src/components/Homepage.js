4e53fbdcccb468d3d26f88c89a708664f27e2098
Ninte <ninte.dev@gmail.com>
2021-07-30 14:53:28 +0100

    Modified homepage component.

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 class Homepage extends React.Component {
   constructor(props) {
@@ -26,17 +27,26 @@ class Homepage extends React.Component {
   }
 
   render() {
-    const titleText = this.state.hasLoaded
-      ? this.state.title
-      : 'Loading title ...';
+    const titleText = this.state.hasLoaded ? (
+      this.state.title
+    ) : (
+      <em>
+        <i className="bi bi-info-square-fill mx-2"></i>Loading ...
+      </em>
+    );
 
     return (
       <div className="container-fluid">
-        <div className="row" id="homepage">
+        <div className="row text-center">
           <div className="col-md-2"></div>
           <div className="col col-md-8">
-            <h1 className="text-color">{titleText}</h1>
+            <h2>{titleText}</h2>
             <div className="my-5"></div>
+
+            <Link to={`/staff`} className="link-alt mb-3">
+              <span>Staff directory</span>
+              <i className="ms-2 bi bi-arrow-right-square-fill text-light"></i>
+            </Link>
           </div>
           <div className="col-md-2"></div>
         </div>
