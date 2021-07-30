7a2849873158c221f0c6f0e9e9e75ca3e7569981
Ninte <ninte.dev@gmail.com>
2021-07-30 11:19:56 +0100

    Setup homepage styling. Inserted dynamic title.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,49 @@
+import React from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return <div></div>;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      hasLoaded: false,
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get('https://staff-clocking-system.herokuapp.com')
+      .then((res) => {
+        this.setState({
+          title: res.data,
+          hasLoaded: true,
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+        console.log('Error fetching title');
+      });
+  }
+
+  render() {
+    const titleText = this.state.hasLoaded
+      ? this.state.title
+      : 'Loading title ...';
+
+    return (
+      <div className="container-fluid">
+        <div className="row" id="homepage">
+          <div className="col-md-2"></div>
+          <div className="col col-md-8">
+            <h1 className="text-color">{titleText}</h1>
+            <div className="my-5"></div>
+          </div>
+          <div className="col-md-2"></div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
