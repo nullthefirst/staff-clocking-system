5503f458d656d530c807010f19c3b518592e251c
Ninte <ninte.dev@gmail.com>
2021-07-30 16:59:09 +0100

    Integrated application routes.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
 import Homepage from './components/Homepage';
+import StaffCreation from './components/StaffCreate';
+import StaffUpdate from './components/StaffUpdate';
 import StaffDetails from './components/StaffDetails';
 import StaffDirectory from './components/StaffDirectory';
+import ClockIn from './components/ClockIn';
+import ClockOut from './components/ClockOut';
 
 class App extends React.Component {
   render() {
@@ -14,6 +18,10 @@ class App extends React.Component {
           <Route exact path="/" component={Homepage} />
           <Route path="/staff" component={StaffDirectory} />
           <Route path="/info/:id" component={StaffDetails} />
+          <Route path="/create" component={StaffCreation} />
+          <Route path="/update/:id" component={StaffUpdate} />
+          <Route path="/clock-in" component={ClockIn} />
+          <Route path="/clock-out" component={ClockOut} />
         </div>
       </Router>
     );
